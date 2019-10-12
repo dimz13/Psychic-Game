5bb1eb6ed23dcf6d15e85e238d86e4d611ee8d4d
dimple shanbhag <dimpleshanbhag@dimples-MacBook-Pro.local>
2019-10-12 16:49:49 -0700

    added readme and changed variable names

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Psychic-Game
+
+# README for Psychic Game
+
+
+## Logic of the game
+    The player has 9 chances to guess the letter the computer has picked.
+    If player failed to guess then the losses counter increments and Guesses left counter is reset for a new guessing battle.
+    If player is awesome and guessed correctly then the wins counter increments and you get another chance to beat the computer.
+
+## Tools Used
+
+- [Visual Studio Code](#vscode)
+- [Chrome Browser](#chrome)
+- [GitLab](#gitlab)
+- [GitHub](#github)
+- [W3Schools](#w3schools)
+- [SlackForum] (#slackforum)
+
+
+## Core Technologies Used
+ - [HTML] (#html)
+ - JavaScript
+ - JQuery
+ - BootStrap CSS Framework
+
+
+
--- assets/js/game.js
+++ assets/js/game.js
@@ -5,19 +5,19 @@
     var userGuess;
     var winsDom = document.getElementById("wins")
     var lossesDom = document.getElementById("losses")
-    var leftDom = document.getElementById("left")
+    var remainGuessesDom = document.getElementById("remainGuesses")
     var guessChoicesDom = document.getElementById("guessChoices")
 
     var computerList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
     var wins = 0;
     var losses = 0;
-    var left = 9;
+    var remainGuesses = 9;
     var guessChoices = []
 
     winsDom.textContent = "Wins: " + wins;
     lossesDom.textContent = "Losses: " + losses;
-    leftDom.textContent = "Guesses Left: " + left;
+    remainGuessesDom.textContent = "Guesses Left: " + remainGuesses;
     guessChoicesDom.textContent = " You guesses so far: " + "";
 
     document.onkeyup = function (event) {
@@ -27,15 +27,15 @@
         console.log(userGuess, computerGuess)
 
         if (userGuess != computerGuess) {
-            left--;
-            console.log("Your choice did not match ", left)
-            leftDom.textContent = "Guesses Left: " + left;
+            remainGuesses--;
+            console.log("Your choice did not match ", remainGuesses)
+            remainGuessesDom.textContent = "Guesses Left: " + remainGuesses;
             guessChoices[guessChoices.length] = userGuess;
             console.log(guessChoices)
             guessChoicesDom.textContent = " You guesses so far: " + guessChoices.join(", ") + "";
-            if (left === 0) {
-                left = 9;
-                leftDom.textContent = "Guesses Left: " + left;
+            if (remainGuesses === 0) {
+                remainGuesses = 9;
+                remainGuessesDom.textContent = "Guesses Left: " + remainGuesses;
                 losses++;
                 lossesDom.textContent = "Losses: " + losses;
                 guessChoicesDom.textContent = " You guesses so far: " + "";
@@ -45,8 +45,8 @@
             wins++
             console.log("You win ", wins)
             winsDom.textContent = "Wins: " + wins;
-            left = 9;
-            leftDom.textContent = "Guesses Left: " + left;
+            remainGuesses = 9;
+            remainGuessesDom.textContent = "Guesses Left: " + remainGuesses;
             lossesDom.textContent = "Losses: " + losses;
             guessChoices = [];
         }
